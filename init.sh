45bbdc073be1c9618b2b95f75c168199a48b565f
Remi Ferrand <remi.ferrand@cc.in2p3.fr>
2018-08-20 08:01:51 +0200

    Update init script

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 export KRB5_CONFIG
 export KRB5_KTNAME
 
-exec /usr/sbin/remctld -F -f ${REMCTLD_CONFIG} -S -s ${SERVICE_PRINCIPAL} -d
+exec /usr/sbin/remctld -m -F -f ${REMCTLD_CONFIG} -S -s ${SERVICE_PRINCIPAL} -d
